b15f51dc1d7ee4464a2d40ebbda85ad65eda495a
Annas Atif <79974531+DCNSPECTRE@users.noreply.github.com>
2025-09-29 17:35:16 +0800

    Changed the FileIO to use Structs

--- fileimport.c
+++ fileimport.c
@@ -1,49 +1,36 @@
 #include "fileimport.h"
 
-int** importMap(const char* filepath){
-
-    const char* fileName = filepath;
-
-    /*Defines a file pointer, and then defines the value of the file pointer */
-    const char * fileName = argv[1];
-    filePtr = fopen(fileName, "r");
-
-    /*If the pointer to the file is null, then it returns an error.*/
-    if(filePtr == NULL){
-        printf("Error openiing file!\n");
-        return 1;
+gameMapInfo* importMap(const char * mapPath){
+    FILE * mapPtr = fopen(mapPath, "r");
+    if(mapPath == NULL){
+        printf("Enter map path, or check directory\n");
+        return NULL;
     }
 
-    printf("File '%s' opened.\n\n Creating Array...", fileName);
-
-    /*Define the integers required to make a 2D malloc'd array*/
-
-    int r, c, i;
-
-    /*The above defined integers are assigned a value from the first two numbers in the map file*/
-
-    fscanf(filePtr, "%d %d", &r, &c);
-
-    /*Allocation of the first dimension of the array*/
-    int ** mapMatrix = malloc(r * sizeof(int*));
-    
-    /*Allocation of the second dimension of the file*/
-    for(i = 0; i < r; i++){
-        mapMatrix[i] = malloc(c * sizeof(int));
+    gameMapInfo * gameMap = malloc(sizeof(gameMapInfo));
+    if(gameMap = NULL){
+        fclose(mapPtr);
+        return NULL;
     }
 
-    printf("Array Created with Dimensions ('%d' x '%d'). Map Generation in Progress.\n", r, c);
-    
-    /*Assigning each value of the array with a value from the map file*/
-    int j;
-    for(i = 0; i < r; i++){
-        for(j = 0; j < c; j++){
-            fscanf(filePtr, "%d", &mapMatrix[i][j]);
+    fscanf(mapPtr, "%d %d", &gameMap->rows, &gameMap->cols);
+
+    gameMap->mapMatrix = malloc(gameMap->rows * sizeof(int*));
+
+    int i, j;
+
+    for(i = 0; i < gameMap->rows; i++){
+        gameMap->mapMatrix[i] = malloc(gameMap->cols * sizeof(int));
+    }
+
+    for(i = 0; i < gameMap->rows; i++){
+        for(j = 0; j < gameMap->cols; j++){
+            fscanf(mapPtr, "%d", &gameMap->mapMatrix[i][j]);
         }
     }
-    printf("Map Generation Completed.\n");
 
-    fclose(filePtr)
-    /*Returns the address to the map matrix*/
-    return mapMatrix;
+    fclose(mapPtr);
+
+    return gameMap;
 }
+
--- fileimport.h
+++ fileimport.h
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include <strings.h>
 #include <stdlib.h>
+
+typedef struct{
+    int rows;
+    int cols;
+    int ** mapMatrix;
+}gameMapInfo;
